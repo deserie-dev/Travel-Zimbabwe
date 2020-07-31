a11a5cbced64b02f7a247f4225b37b2c6a785d59
Deserie Murembeni <deserie.dev@gmail.com>
2020-08-01 01:04:29 +0200

    Added Jest tests

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 
-const app = express()
+const handlers = require('./lib/handlers')
 
-app.use(express.static(__dirname + 'public'))
+const app = express()
 
 //Configure Handlebars view engine
 app.engine('handlebars', expressHandlebars({
@@ -16,25 +16,25 @@ app.use(express.static(__dirname + '/public'))
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req,res) => res.render('home'))
+app.use(express.static(__dirname + 'public'))
 
-app.get('about', (req,res) => res.render('about'))
+app.get('/', handlers.home)
+
+app.get('about', handlers.home)
 
 //custom 404 page
-app.use((res,req) => {
-    res.status(404)
-    res.render('404')
-})
+app.use(handlers.notFound)
 
 //custom 500 page
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
-app.listen(port, () => console.log(
-    `Express started on http://localhost:${port}; ` +
-    `press Ctrl-C to terminate.`))
-
+if(require.main === module) {
+    app.listen(port, () => {
+      console.log( `Express started on http://localhost:${port}` +
+        '; press Ctrl-C to terminate.' )
+    })
+  } else {
+    module.exports = app
+  }
+  
 
